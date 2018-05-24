11861922e93ec41338e645a1ad84ca8e27c6d7b2
Luiz Henrique Freire Barros <luizhenriquefbb@gmail.com>
2018-05-24 10:34:41 -0300

    move .py files to core folder

---
+++ core/pre-processing/trainning.py
@@ -0,0 +1,42 @@
+'''
+Run trainning
+
+go to core folder and run 'python pre-processing/trainning.py'
+'''
+import os, sys
+sys.path.append("../core")
+import ImageToAscii
+
+def main():
+	'''
+	Steps:
+
+	Read all images of "testeCases/" folder catalogging by their names
+	i.e: image A.png contain the best case of an "A" char, so save it into the KNN
+
+	KNN will be a ordered matrix nX2
+		n == number of tests char
+		2 = ascii art + corresponding char
+
+	Do not read from "testeCases/unknown/". This images will be use to test the KNN
+	'''
+
+	# Read all images of "testeCases/" folder catalogging by their names
+	folder = "../testeCases"
+
+	paths = [os.path.join(folder, nome) for nome in os.listdir(folder)]
+	files = [arq for arq in paths if os.path.isfile(arq)]
+	pngs = [arq for arq in files if arq.lower().endswith(".png")]
+
+	
+	# convert each image to ascii and store into matrix
+	matrix = []
+	for imagePath in pngs:
+		asciiImage = ImageToAscii.handle_image_conversion(imagePath)
+		imageName = imagePath.replace(folder+'/', '').replace('.png','')
+		matrix.append([asciiImage, imageName])
+
+
+
+if __name__ == '__main__':
+	main()
